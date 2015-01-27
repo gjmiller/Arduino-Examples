c80ffaa63b92a88b801c4338feafbfe9f10ca31f
gjmiller <gjmiller@gmail.com>
2015-01-27 18:53:20 -0500

    Added comments.

--- arduinoStructTest/arduinoStructTest.ino
+++ arduinoStructTest/arduinoStructTest.ino
@@ -10,9 +10,12 @@ struct weatherStation
  double lastUpdateMillis;
 };
 
+// Pointer arrays to hold station structs
 struct weatherStation* stationList = (struct weatherStation*) malloc(sizeof(struct weatherStation));
 struct weatherStation* tmpStationList;
+// The number of stations currently in the main list
 int stationCount = 1;
+// Function declarations
 void printStation(struct weatherStation* station);
 
 void setup()
@@ -20,6 +23,7 @@ void setup()
   // Serial init
   Serial.begin(9600);
   Serial.println("Setup start");
+  // Setup first station
   stationList[0].temperature = 98.6;
   stationList[0].humidity = 34;
   stationList[0].pressure = 56;
@@ -27,18 +31,24 @@ void setup()
   Serial.println("Setup done");
   printStation(&stationList[0]);
   Serial.println("=================");
+  // Increment station count and reallocate memory to prepare for the addition
+  // of the second station
   stationCount++;
   tmpStationList = (struct weatherStation*) realloc(stationList, stationCount * sizeof(struct weatherStation));
+  // If the temporary pointer array is not NULL, then reallocation is successful and the main array
+  // needs to be repointed to the temp pointer array.
   if (tmpStationList != NULL)
   {
     Serial.println(" Resized array successfully");
     stationList = tmpStationList;
   }
+  // If tmpStationList is NULL, then memory was not successfully allocated and we have an issue.
   else
   {
     Serial.println(" Did not resize array successfully");
     // Problem!
   }
+  // Setup second station record.
   stationList[1].temperature = 68;
   stationList[1].humidity = 45;
   stationList[1].pressure = 67;
@@ -48,9 +58,10 @@ void setup()
 
 void loop() 
 {
-
+ // Do nothing here. This space left intentionally blank.
 }
 
+// Print station struct's data members.
 void printStation(struct weatherStation* station)
 {
  Serial.println(station->name);
